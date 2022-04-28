18b70b1706372984df7e8ba5be5ad194ef698aa6
Janitor-Bob <62737046+Janitor-Bob@users.noreply.github.com>
2022-04-28 05:20:26 -0400

    split works but nothing else does. this push is a real big meme

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,10 +15,14 @@ public class GameManager : MonoBehaviour
     public Button splitBtn;
 	public Button insuranceBtn;
     public Button betBtn;
+	
+	
+	//Game Objects
+	public CardScript cardScript;   
+	public DeckScript deckScript;
 
-    // Access the player and dealer's script
-    public PlayerScript playerScript; //OG playerscript, grabbed from game
-    public PlayerScript dealerScript; //same as above but for the dealer
+    //Dealer script
+    public PlayerScript dealerScript; 
 	
 	//Active player list
 	public PlayerHelper playerList;
@@ -34,7 +38,8 @@ public class GameManager : MonoBehaviour
 
     // Card hiding dealer's 2nd card
     public GameObject hideCard;
-
+	public GameObject playerStartCard;
+	public GameObject dealerStartCard;
     private int bank = 0;
 	int round_base_bet = 0;
 
@@ -67,9 +72,10 @@ public class GameManager : MonoBehaviour
         splitBtn.gameObject.SetActive(false);
         insuranceBtn.gameObject.SetActive(false);
         hideCard.GetComponent<Renderer>().sortingOrder = 999;
-        playerScript.deckScript.SetDecks(decks);
-        playerScript.deckScript.GetCardValues();
-		playerList = new PlayerHelper(playerScript);
+        deckScript.SetDecks(decks);
+        deckScript.GetCardValues();
+		playerList = new PlayerHelper(new PlayerScript(cardScript, deckScript, playerStartCard));
+		dealerScript = new PlayerScript(cardScript, deckScript, dealerStartCard, 0); 
     }
 
     private void DealClicked()
@@ -153,10 +159,10 @@ public class GameManager : MonoBehaviour
 	
 	private void SplitClicked()//need to add checking if number of cards in hand is two and if cards are the same
 	{
-		/*PlayerScript temp = new PlayerScript();
+		PlayerScript temp = new PlayerScript(cardScript, deckScript, playerStartCard);
 		temp.bet = round_base_bet;
 		temp.offset = playerList.data.offset +1;
-		playerList.addPlayer(temp);*/
+		playerList.addPlayer(temp);
 	}
 
     private void HitDealer()
@@ -264,6 +270,10 @@ public class GameManager : MonoBehaviour
         }*/
     }
 	
+	
+	
+	
+	
 	public class PlayerHelper
 	{
 		public PlayerHelper previous;
@@ -280,6 +290,117 @@ public class GameManager : MonoBehaviour
 		
 		public void addPlayer(PlayerScript player){
 			this.next = new PlayerHelper(player, this);
+		}
+	}
+	
+	
+	
+	
+	public class PlayerScript
+	{
+		// --- This script is for BOTH player and dealer
+
+		// Get other scripts
+		public CardScript cardScript;   
+		public DeckScript deckScript;
+
+		// Total value of player/dealer's hand
+		public int handValue = 0;
+		public bool softCount = false;
+		
+		
+		public int bet = 0;
+		public int offset = 0;
+		
+		//is player a split instance
+		public bool split = false;
+		
+		// Array of card objects on table
+		private GameObject[] hand = new GameObject[10];
+		public GameObject startCard;
+		public int dir;
+
+		// Index of next card to be turned over
+		public int cardIndex = 0;
+		// Tracking aces for 1 to 11 conversions
+		List<CardScript> aceList = new List<CardScript>();
+		
+		public PlayerScript(CardScript card, DeckScript deck, GameObject c, int d = 1){
+			this.cardScript = card;
+			this.deckScript = deck;
+			this.startCard = c;
+			this.dir = d;
+		}
+		
+		
+		public void StartHand()
+		{
+			GetCard();
+			GetCard();
+		}
+
+		// Add a hand to the player/dealer's hand
+		public int GetCard()
+		{
+			// Get a card, use deal card to assign sprite and value to card on table
+			// GameObject temp = Instantiate(startCard, new Vector3(cardIndex * 20f, cardIndex * 20f, 0), Quaternion.identity);
+			int cardValue;
+			GameObject temp = Instantiate(startCard);
+			temp.transform.Translate(new Vector3(offset*1f + cardIndex * 0.5f, cardIndex * dir * 0.5f-1.05f, 0));
+			hand[cardIndex] = temp;
+			cardValue = deckScript.DealCard(hand[cardIndex].GetComponent<CardScript>());
+			
+			// Show card on game screen
+			hand[cardIndex].GetComponent<Renderer>().enabled = true;
+			// Add card value to running total of the hand
+			handValue += cardValue;
+			// If value is 1, it is an ace
+			if(cardValue == 1)
+			{
+				aceList.Add(hand[cardIndex].GetComponent<CardScript>());
+			}
+			// Check if we should use an 11 instead of a 1
+			AceCheck();
+			cardIndex++;
+			return handValue;
+		}
+
+		// Search for needed ace conversions, 1 to 11 or vice versa
+		public void AceCheck()
+		{
+			// for each ace in the list check
+			foreach (CardScript ace in aceList)
+			{
+				if(handValue + 10 < 22 && ace.GetValueOfCard() == 1)
+				{
+					// if converting, adjust card object value and hand
+					ace.SetValue(11);
+					handValue += 10;
+					softCount = true;
+				} else if (handValue > 21 && ace.GetValueOfCard() == 11)
+				{
+					// if converting, adjust gameobject value and hand value
+					ace.SetValue(1);
+					handValue -= 10;
+					softCount = false;
+				}
+			}
+		}
+
+		// Hides all cards, resets the needed variables
+		public void ResetHand()
+		{
+			for(int i = 0; i < hand.Length; i++)
+			{
+				if (hand[i] != null) 
+				{
+					hand[i].GetComponent<CardScript>().ResetCard();
+					hand[i].GetComponent<Renderer>().enabled = false;
+				}
+			}
+			cardIndex = 0;
+			handValue = 0;
+			aceList = new List<CardScript>();
 		}
 	}
 }
